c2c17c2cef81ec7ca5d31a13a1036355739eb85c
hamza-ridhwan <hamzathehamzah@gmail.com>
2025-02-02 17:57:38 +0300

    Updated the layout file

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -5,9 +5,9 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="min-h-[92vh] bg-gray-200 container mx-auto p-8">
+      <div className="flex-grow bg-gray-200 container mx-auto p-8">
         <Outlet />
       </div>
       <Footer />
